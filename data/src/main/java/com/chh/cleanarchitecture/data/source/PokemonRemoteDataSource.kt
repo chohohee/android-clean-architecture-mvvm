732d40333ccb48f45634e6fb053435cd059784c2
chohohee <gone1005@gmail.com>
2024-11-07 14:11:17 +0900

    Add remote paging mediator

--- data/src/main/java/com/chh/cleanarchitecture/data/source/PokemonRemoteDataSource.kt
+++ data/src/main/java/com/chh/cleanarchitecture/data/source/PokemonRemoteDataSource.kt
@@ -1,6 +1,7 @@
 package com.chh.cleanarchitecture.data.source
 
 import com.chh.cleanarchitecture.data.model.PokemonPagedData
+import com.chh.cleanarchitecture.data.util.Result
 
 interface PokemonRemoteDataSource {
 
--- remote/src/main/java/com/chh/cleanarchitecture/remote/source/DefaultPokemonRemoteDataSource.kt
+++ remote/src/main/java/com/chh/cleanarchitecture/remote/source/DefaultPokemonRemoteDataSource.kt
@@ -4,11 +4,15 @@ import com.chh.cleanarchitecture.data.model.PokemonPagedData
 import com.chh.cleanarchitecture.data.source.PokemonRemoteDataSource
 import com.chh.cleanarchitecture.remote.api.PokemonApi
 import javax.inject.Inject
+import com.chh.cleanarchitecture.data.util.Result
+import com.chh.cleanarchitecture.data.util.safeApiCall
 
 internal class DefaultPokemonRemoteDataSource @Inject constructor(
     private val pokemonApi: PokemonApi
 ) : PokemonRemoteDataSource {
 
     override suspend fun fetchPokemonPaged(limit: Int, offset: Int): Result<PokemonPagedData> =
-        kotlin.runCatching { pokemonApi.fetchPokemonPaged(limit, offset).toData() }
+        safeApiCall {
+            pokemonApi.fetchPokemonPaged(limit, offset).toData()
+        }
 }
