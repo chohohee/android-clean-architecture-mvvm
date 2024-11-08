846ed545d3bf5c176d3fc1c500ee08aa304fe57e
chohohee <gone1005@gmail.com>
2024-11-08 12:31:04 +0900

    Add pokemon fragment navigation

--- presentation/src/main/java/com/chh/cleanarchitecture/presentation/MainActivity.kt
+++ presentation/src/main/java/com/chh/cleanarchitecture/presentation/MainActivity.kt
@@ -1,22 +1,17 @@
 package com.chh.cleanarchitecture.presentation
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.chh.cleanarchitecture.presentation.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
---
+++ presentation/src/main/java/com/chh/cleanarchitecture/presentation/ui/pokemon/PokemonFragment.kt
@@ -0,0 +1,13 @@
+package com.chh.cleanarchitecture.presentation.ui.pokemon
+
+import com.chh.cleanarchitecture.presentation.R
+import com.chh.cleanarchitecture.presentation.databinding.FragmentPokemonBinding
+import com.chh.cleanarchitecture.presentation.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class PokemonFragment : BaseFragment<FragmentPokemonBinding>(R.layout.fragment_pokemon) {
+
+    override fun initView() {
+    }
+}
