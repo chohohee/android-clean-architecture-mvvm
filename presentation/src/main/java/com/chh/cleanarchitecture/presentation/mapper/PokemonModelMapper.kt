e284240e86a7ea41def7be8796e6feacc9b64048
chohohee <gone1005@gmail.com>
2024-11-08 12:41:25 +0900

    Add domain to model mapper

---
+++ presentation/src/main/java/com/chh/cleanarchitecture/presentation/mapper/PokemonModelMapper.kt
@@ -0,0 +1,11 @@
+package com.chh.cleanarchitecture.presentation.mapper
+
+import com.chh.cleanarchitecture.domain.model.Pokemon
+import com.chh.cleanarchitecture.presentation.model.PokemonModel
+
+
+internal fun Pokemon.toPresentation(): PokemonModel =
+    PokemonModel(
+        name = this.name,
+        thumbnailUrl = this.getThumbnailUrl()
+    )
