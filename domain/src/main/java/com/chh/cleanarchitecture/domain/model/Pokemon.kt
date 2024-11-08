d92a4efc368e8d9f5a35356b39927696a6009e2b
chohohee <gone1005@gmail.com>
2024-11-08 12:36:22 +0900

    Update image path from url

--- domain/src/main/java/com/chh/cleanarchitecture/domain/model/Pokemon.kt
+++ domain/src/main/java/com/chh/cleanarchitecture/domain/model/Pokemon.kt
@@ -2,5 +2,12 @@ package com.chh.cleanarchitecture.domain.model
 
 data class Pokemon(
     val name: String,
-    val url: String,
-)
+    val url: String
+) {
+
+    private fun getIndex(): String = url.split("/".toRegex()).dropLast(1).last()
+
+    fun getThumbnailUrl(): String =
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${getIndex()}.png"
+
+}
