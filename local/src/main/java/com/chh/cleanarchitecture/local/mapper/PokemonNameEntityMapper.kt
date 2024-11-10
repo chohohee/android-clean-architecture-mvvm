31bbb83b7bcd97addb8238a331f3d668c5e8b912
chohohee <gone1005@gmail.com>
2024-11-10 14:43:38 +0900

    Add name entity mapper

---
+++ local/src/main/java/com/chh/cleanarchitecture/local/mapper/PokemonNameEntityMapper.kt
@@ -0,0 +1,16 @@
+package com.chh.cleanarchitecture.local.mapper
+
+import com.chh.cleanarchitecture.data.model.PokemonNameData
+import com.chh.cleanarchitecture.local.model.PokemonNameEntity
+
+internal fun PokemonNameEntity.toData(): PokemonNameData =
+    PokemonNameData(
+        name = this.name,
+        names = this.names
+    )
+
+internal fun PokemonNameData.toEntity(): PokemonNameEntity =
+    PokemonNameEntity(
+        name = this.name,
+        names = this.names
+    )
