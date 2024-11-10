4337c1d1510946d1d318a275ccb0a6f752f5d2b1
chohohee <gone1005@gmail.com>
2024-11-10 14:54:21 +0900

    Add data to domain

--- data/src/main/java/com/chh/cleanarchitecture/data/model/PokemonNameData.kt
+++ data/src/main/java/com/chh/cleanarchitecture/data/model/PokemonNameData.kt
@@ -1,6 +1,22 @@
 package com.chh.cleanarchitecture.data.model
 
+import com.chh.cleanarchitecture.data.mapper.DataToDomainMapper
+import com.chh.cleanarchitecture.domain.model.Name
+import com.chh.cleanarchitecture.domain.model.PokemonName
+
 data class PokemonNameData(
     val name: String,
     val names: List<NameData>
-)
+) : DataToDomainMapper<PokemonName> {
+    override fun toDomain(): PokemonName =
+        PokemonName(
+            name = this.name,
+            names = this.names.map {
+                Name(
+                    language = it.language,
+                    name = it.name
+                )
+            }
+        )
+}
+
