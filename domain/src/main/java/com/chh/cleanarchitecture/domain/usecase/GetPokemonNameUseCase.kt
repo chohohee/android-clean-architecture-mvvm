df27c70ecd5060e0552e90c343eacea3b6180bdb
chohohee <gone1005@gmail.com>
2024-11-11 14:32:19 +0900

    Update localization of pokemon names

--- domain/src/main/java/com/chh/cleanarchitecture/domain/usecase/GetPokemonNameUseCase.kt
+++ domain/src/main/java/com/chh/cleanarchitecture/domain/usecase/GetPokemonNameUseCase.kt
@@ -1,13 +1,19 @@
 package com.chh.cleanarchitecture.domain.usecase
 
-import com.chh.cleanarchitecture.domain.model.PokemonName
 import com.chh.cleanarchitecture.domain.repository.PokemonRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class GetPokemonNameUseCase @Inject constructor(
-    private val pokemonRepository: PokemonRepository
+    private val pokemonRepository: PokemonRepository,
+    private val getLocalizedNameUseCase: GetLocalizedNameUseCase
 ) {
 
-    suspend operator fun invoke(name: String): PokemonName = pokemonRepository.getPokemonName(name)
-
+    operator fun invoke(name: String): Flow<String> =
+        flow {
+            val pokemonName = pokemonRepository.getPokemonName(name)
+            val localizedName = getLocalizedNameUseCase(pokemonName.names)
+            emit(localizedName ?: name)
+        }
 }
