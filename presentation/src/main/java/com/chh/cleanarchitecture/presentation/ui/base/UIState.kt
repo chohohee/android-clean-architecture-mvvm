0f34e4b5dd3001f34c3ccd0f56fb1c9f06a2610f
chohohee <gone1005@gmail.com>
2024-11-12 10:21:00 +0900

    Update nothing state

--- presentation/src/main/java/com/chh/cleanarchitecture/presentation/ui/base/UIState.kt
+++ presentation/src/main/java/com/chh/cleanarchitecture/presentation/ui/base/UIState.kt
@@ -1,6 +1,7 @@
 package com.chh.cleanarchitecture.presentation.ui.base
 
 sealed interface UiState {
+    data object Nothing : UiState
     data object Loading : UiState
     data object Success : UiState
     data class Error(val error: Throwable?) : UiState
