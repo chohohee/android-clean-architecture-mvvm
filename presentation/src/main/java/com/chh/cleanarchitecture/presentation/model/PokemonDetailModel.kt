a4922207a0bd02cf0dea17586c65e13413c6c699
chohohee <gone1005@gmail.com>
2024-11-13 11:40:35 +0900

    Update pokemon number

--- presentation/src/main/java/com/chh/cleanarchitecture/presentation/model/PokemonDetailModel.kt
+++ presentation/src/main/java/com/chh/cleanarchitecture/presentation/model/PokemonDetailModel.kt
@@ -1,9 +1,14 @@
 package com.chh.cleanarchitecture.presentation.model
 
+import java.util.Locale
+
 data class PokemonDetailModel(
     val id: Int,
     val name: String,
     val height: Int,
     val weight: Int,
     val types: List<PokemonTypeModel>
-)
+) {
+
+    fun getNo(): String = String.format(Locale.getDefault(),"#%03d", id)
+}
