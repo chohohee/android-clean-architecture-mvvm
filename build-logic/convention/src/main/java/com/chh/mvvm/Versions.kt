83a09295ac6bd4e5a261362de01061374673d052
chohohee <gone1005@gmail.com>
2024-11-14 09:58:34 +0900

    Update java, jvm version 21

--- build-logic/convention/src/main/java/com/chh/mvvm/Versions.kt
+++ build-logic/convention/src/main/java/com/chh/mvvm/Versions.kt
@@ -7,6 +7,6 @@ object Versions {
     const val COMPILE_SDK_VERSION = 35
     const val MIN_SDK_VERSION = 24
     const val TARGET_SDK_VERSION = 34
-    val JAVA_VERSION = JavaVersion.VERSION_17
-    val JVM_TARGET = JvmTarget.JVM_17
+    val JAVA_VERSION = JavaVersion.VERSION_21
+    val JVM_TARGET = JvmTarget.JVM_21
 }
